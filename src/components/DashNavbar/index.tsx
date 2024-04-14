f00501e147fdd774625d82672cb5a4bd46bf9a04
Vicolas11 <incrediblechamp1@gmail.com>
2024-04-14 16:37:50 +0100

    style: modified responsive styling

--- src/components/DashNavbar/index.tsx
+++ src/components/DashNavbar/index.tsx
@@ -5,17 +5,30 @@ import { IoMdSettings } from "react-icons/io";
 import styles from "./styles.module.scss";
 import { FaUser } from "react-icons/fa";
 import { constant } from "../../configs/constant.config";
+import { sliceText } from "../../utils/slicetext.util";
 
 interface Props {
   title: string;
   userData: UserData | null;
 }
 
+const width = window.innerWidth;
+
 export default function DashNavbar({ title, userData }: Props) {
   const setSearchParams = useSearchParams()[1];
   const navigate = useNavigate();
   const { adminEmail } = constant;
   const isSuperAdmin = userData?.email === adminEmail;
+
+  const shrinkTxt = (txt: string) => {
+    return width <= 312
+      ? sliceText(txt, 5)
+      : width <= 360
+      ? sliceText(txt, 10)
+      : width < 412
+      ? sliceText(txt, 15)
+      : txt;
+  };
 
   return (
     <nav className={styles.dashNavbar}>
@@ -35,7 +48,7 @@ export default function DashNavbar({ title, userData }: Props) {
         </span>
         <div className={styles.title}>
           <div className={styles.name}>
-            <h4>{userData?.email || "user@email.com"} </h4>
+            <h4>{shrinkTxt(userData?.email as string) || "user@email.com"} </h4>
             <h6>
               {isSuperAdmin ? "SuperAdmin" : userData?.role || "UserRole"}{" "}
             </h6>
